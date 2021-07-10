a274d39e107ecc6c56b2309095a2c40e1d020983
jakeiridion <emil.ej.jeleskovic@gmail.com>
2021-07-10 15:51:32 +0200

    database functions

--- database/database.go
+++ database/database.go
@@ -1,7 +1,39 @@
 package database
 
+import (
+	"log"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
 type Product struct {
 	Name  string `json:"name"`
 	Price int    `json:"price"`
 	Image string `json:"image"`
+}
+
+func OpenDatabase() (*sql.DB, error){
+	db, err := sql.Open("mysql",
+		"user:password@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db, err
+}
+
+func CloseDatabase(db *sql.DB){
+	db.Close()
+}
+
+func Search(db *sql.DB, name string) (Product){
+	results, err := db.Query("SELECT * FROM products WHERE name LIKE '%?%'", name)
+    if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&name, &price, &image)
+		if err != nil {
+			log.Fatal(err)
+		}
 }
--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"net/http"
 	"time"
 
+	"grace-hopper/database"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
+	db, err := open_database()
 
 	router.GET("/", func(c *gin.Context) {
 		datetime := time.Now().Format("01-02-2006 15:04:05")
