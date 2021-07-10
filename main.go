693a220418105afa91fd81ff9816c14629c34079
jakeiridion <emil.ej.jeleskovic@gmail.com>
2021-07-10 16:14:42 +0200

    updated to work with database.go

--- main.go
+++ main.go
@@ -11,7 +11,10 @@ import (
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
-	db, err := open_database()
+	db, err := database.OpenDatabase()
+	
+	defer database.CloseDatabase(db)
+	// handle Error
 
 	router.GET("/", func(c *gin.Context) {
 		datetime := time.Now().Format("01-02-2006 15:04:05")
